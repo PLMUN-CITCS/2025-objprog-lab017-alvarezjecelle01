1eed7fbf82cc21c6dcf7a712f41b703cd0f7a49c
alvarezjecelle01 <alvarezjecelle_bsit@plmun.edu.ph>
2025-03-19 19:54:50 +0800

    Submitting OBJPROG Week 05 - Laboratory # 17

---
+++ GradeCalculator.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class GradeCalculator {
+    
+    // Method to get the student's score from the user
+    public static double getStudentScore() {
+        Scanner scanner = new Scanner(System.in);
+        double score;
+
+        // Prompt the user
